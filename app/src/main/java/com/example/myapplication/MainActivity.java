11e34719cf397a2670df1c42554260df6d884c79
Ali-Han Ö. <Ali-Han.Oezcelik@rmg.com>
2024-10-18 14:50:49 +0200

    Test ob Commit auf Master funktioniert.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
+    private popelInDerNaseFuerTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
